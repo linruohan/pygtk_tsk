a2818bdc96059d9b7b408005835a9070da3b9460
linruohan <mjt1220@126.com>
2024-08-18 15:20:12 +0800

    pygtk4 add first

---
+++ main.py
@@ -0,0 +1,36 @@
+import sys
+import gi
+gi.require_version('Gtk', '4.0')
+gi.require_version('Adw', '1')
+from gi.repository import Gtk, Adw
+
+
+
+
+class MainWindow(Gtk.ApplicationWindow):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Things will go here
+        self.box1 = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
+        self.set_child(self.box1)
+
+        self.button = Gtk.Button(label="Hello")
+        self.box1.append(self.button)
+        self.button.connect('clicked', self.hello)
+
+    def hello(self,button):
+        print("Hello world")
+
+
+class MyApp(Adw.Application):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.win = None
+        self.connect('activate', self.on_activate)
+
+    def on_activate(self, app):
+        self.win = MainWindow(application=app)
+        self.win.present()
+
+app = MyApp(application_id="com.example.GtkApplication")
+app.run(sys.argv)
